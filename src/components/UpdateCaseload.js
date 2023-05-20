9923ef85c58f3d1a537a6d33c87e9e2a187fd95e
Staci Rosales <stacileep2@gmail.com>
2023-05-20 13:40:59 -0500

    Update components with caseload form

---
+++ src/components/UpdateCaseload.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function UpdateCaseload() {
+
+  return (
+    <div classname="form">
+      <form className="form">
+        <label>Client Name:</label>
+        <input type="text"></input>
+        <br />
+        <label>Therapist:</label>
+        <input type="text"></input>
+        <br />
+        <label>Location:</label>
+        <input type="text"></input>
+        <br />
+        <label>Eligibility:</label>
+        <input type="text"></input>
+        <br />
+        <label>Minutes Owed:</label>
+        <input type="text"></input>
+      </form>
+    </div>
+  )
+}
+
+export default UpdateCaseload;
